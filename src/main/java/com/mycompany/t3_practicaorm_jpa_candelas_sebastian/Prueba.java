13dcf16dee8610d9ececb738b4aa90d827cc9f2d
SebastianCanQue <sebas150cq@gmail.com>
2025-01-22 17:05:38 +0100

    Commit 0.0

---
+++ src/main/java/com/mycompany/t3_practicaorm_jpa_candelas_sebastian/Prueba.java
@@ -0,0 +1,13 @@
+package com.mycompany.t3_practicaorm_jpa_candelas_sebastian;
+
+/**
+ *
+ * @author Sebastián Candelas Quero
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
